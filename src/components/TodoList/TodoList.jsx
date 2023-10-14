2862b4cb44ceb853c9882060af9f49a5048b04c3
oqoqoh <oqoq59@gmail.com>
2023-10-15 02:54:36 +0900

    Add: TodoList 컴포넌트에 getFilteredItems 함수 추가 및 적용

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import AddTodo from './AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
     const [todos, setTodos] = useState([
         { id: '123', text: 'test1', status: 'active' },
         { id: '124', text: 'test2', status: 'active' },
@@ -12,14 +12,22 @@ export default function TodoList() {
     };
     const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
     const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
+    const filtered = getFilteredItems(todos, filter);
     return (
         <section>
             <ul>
-                {todos.map((todo) => (
+                {filtered.map((todo) => (
                     <Todo key={todo.id} todo={todo} onUpdate={handleUpdate} onDelete={handleDelete} />
                 ))}
             </ul>
             <AddTodo onAdd={handleAdd} />
         </section>
     );
+}
+
+function getFilteredItems(todos, filter) {
+    if (filter === 'all') {
+        return todos;
+    }
+    return todos.filter((todo) => todo.status === filter);
 }
